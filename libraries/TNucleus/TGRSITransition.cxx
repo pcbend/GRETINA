b1db244e707d92f95c76b940e7aeb0c451efc4b7
Peter Bender <benderp@walnut.nscl.msu.edu>
2015-04-01 13:16:18 -0400

    TKinematics / TNucleus added to repo

---
+++ libraries/TNucleus/TGRSITransition.cxx
@@ -0,0 +1,34 @@
+
+#include <TGRSITransition.h>
+
+
+ClassImp(TGRSITransition)
+
+TGRSITransition::TGRSITransition() {
+  Class()->IgnoreTObjectStreamer(true);
+  energy                 = 0.0; 
+  energy_uncertainty    = 0.0; 
+  intensity              = 0.0; 
+  intensity_uncertainty = 0.0;  
+
+}
+
+TGRSITransition::~TGRSITransition() { }
+
+
+
+
+int TGRSITransition::Compare(const TObject *obj) const { 
+//Compares the intensities of the TGRSITransitions and returns
+//-1 if this >  obj
+//0 if  this == obj
+//1 if  this <  obj
+   if(this->intensity > ((TGRSITransition*)obj)->intensity) 
+      return -1;  
+   if(this->intensity == ((TGRSITransition*)obj)->intensity) 
+      return  0;  
+   if(this->intensity < ((TGRSITransition*)obj)->intensity) 
+      return  1;  
+}
+
+
