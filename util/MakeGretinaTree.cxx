de22f27a22a23b594a867444835013d869db3d29
Peter Bender <pcbend@gmail.com>
2015-04-08 09:38:34 -0400

    Weighted positions tweaking....

--- util/MakeGretinaTree.cxx
+++ util/MakeGretinaTree.cxx
@@ -92,7 +92,7 @@ int main(int argc, char **argv) {
       phoswall->FindWeightedPosition();
       gretina->BuildAddBack();
      
-      phoswall->GetPosition().Print();
+      //phoswall->GetPosition().Print();
 
       tree->Fill();      
 
