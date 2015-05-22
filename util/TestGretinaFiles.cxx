25d06eea3314d8330cbb6d5c60b9e40e49e113a0
Peter Bender <pcbend@gmail.com>
2015-05-22 09:52:15 -0400

    snapshot

---
+++ util/TestGretinaFiles.cxx
@@ -0,0 +1,154 @@
+//g++ MakeTree.cxx `grut-config --cflags --libs --root` 
+
+#include <cstdio>
+#include <string>
+
+#include <TGEBMultiFile.h>
+#include <TGEBFile.h>
+#include <TGEBEvent.h>
+
+#include <Globals.h>
+
+#include <Rtypes.h>
+
+#include <TGretina.h>
+#include <TPhosWall.h>
+
+#include <TFile.h>
+#include <TTree.h>
+
+int main(int argc, char **argv) {
+  if(argc<2) {
+    printf("Usage:   MakeGretinaTree <filename.dat/dat.gz/cvt/cvt.gz> -o <outfilename.root>\n");
+    printf("\tas many input files as nessecray may be passed on the commandline.\n");
+    printf("\tin the case of multiple inputs, the GEB events are fromt he files\n"
+           "\ttime ordered. \n");
+    return 1;
+  }
+
+  TGEBMultiFile multi;
+  Long_t First1 = 0; 
+  Long_t First2 = 0;
+
+  std::string outfilename;
+
+  for(int x=1;x<argc;x++) {
+    if(!strcmp(argv[x],"-o") || !strcmp(argv[x],"--outfile")) {
+      if((x+1)>=argc) {
+         printf("outfile flag found, but file name not found.\n");
+         return 1;
+      } 
+      x++;
+      outfilename.assign(argv[x]);
+    } else {
+      multi.Add(argv[x]);
+    }
+  }
+
+  TGEBEvent *event = multi.Read();
+  if(!event) {
+     printf("problem reading inputfiles.\n");
+     return 1;
+  }
+  Long_t LastTime = event->GetTimeStamp();
+  Int_t  LastType = event->GetEventType();
+
+  int stop =0;
+
+  TGretina  *gretina  = new TGretina;
+  TPhosWall *phoswall = new TPhosWall;
+
+  int phoscounter = 0;
+  int gretcounter = 0;
+
+  if(outfilename.length()==0) {
+     printf("\tno outfile file specified, using default: myoutput.root\n");
+     outfilename.assign("myoutput.root");
+  }
+
+
+  if(outfilename.find(".root")==std::string::npos)
+     outfilename.append(".root");
+
+  TFile *outfile = new TFile(outfilename.c_str(),"recreate");
+  TTree *tree = new TTree("Data","Data");
+  TTree::SetMaxTreeSize((Long_t)1*(Long_t)1024*(Long_t)1024*(Long_t)1024);
+  tree->Branch("TGretina","TGretina",&gretina);
+  tree->Branch("TPhosWall","TPhosWall",&phoswall);
+
+  bool run = true;
+  int loopcounter=0;
+
+  while(run) {
+    loopcounter++;
+    event = multi.Read();
+    if(!event) {
+       run =false;
+       break;
+    }   
+    if(event->GetEventType()==8)
+       continue;
+    if(abs(event->GetTimeStamp()-LastTime)>500) {  // 5 us build time.
+      printf(DRED);
+      printf("Build called.\t");
+      std::cout << event->GetTimeStamp() << "  -  " << LastTime << "  =  " << event->GetTimeStamp() - LastTime;
+      printf(RESET_COLOR "\n");
+ 
+
+      //phoswall->FindWeightedPosition();
+      //gretina->BuildAddBack();
+      ////phoswall->GetPosition().Print();
+      //tree->Fill();      
+      //gretina->Clear();
+      //phoswall->Clear();
+    }
+
+
+    switch(event->GetEventType()) {
+      case 1: {
+       //printf("\tfound gretina bank 1\n");
+       std::cout << *((TGEBEvent::TGEBBankType1*)event->GetData());
+        //G2Fragment frag(*event);
+        //frag.Print();
+
+        //TGretinaHit hit(frag);
+        //gretina->AddGretinaHit(hit);
+        gretcounter++;
+      }
+      break;
+      case 17: {
+        PWFragment frag(*event);
+       
+        frag.Print();
+
+        //phoswall->AddPWHit(frag);
+        phoscounter++;
+      }
+      break;
+    };  
+
+    LastTime = event->GetTimeStamp();
+    LastType = event->GetEventType();
+
+
+    if((loopcounter%100000)==0)
+       multi.Print();
+
+  }
+  outfile = tree->GetCurrentFile();
+  outfile->Write();
+  outfile->Close();
+
+
+  return 0;
+
+}
+
+
+
+
+
+
+
+
+
